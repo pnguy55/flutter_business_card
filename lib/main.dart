8642634126603d7e0eb21036a9d022f7cc8a0130
Phi Nguyen <pnguy55@gmail.com>
2020-04-10 01:06:34 -0500

    Add inkwells

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:ui' as prefix0;
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,7 +27,58 @@ class MyApp extends StatelessWidget {
                 radius: 80.0,
                 backgroundImage: AssetImage('assets/rhett.jpg'),
               ),
-              Image.asset('assets/rhett-logo.jpg', height: 80.0)
+              Image.asset('assets/rhett-logo.jpg', height: 80.0),
+              Container(
+                width: 250.0,
+                color: Colors.black,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: InkWell(
+                  onTap: () =>
+                      launch('mailto:https://rhettspanolaw.com/contact/'),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Icon(Icons.mail, color: Colors.white),
+                      SizedBox(
+                        width: 15.0,
+                      ),
+                      Text('Email Me',
+                          style: TextStyle(
+                              fontFamily: 'Quicksand',
+                              fontSize: 25.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                width: 250.0,
+                color: Colors.black,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: InkWell(
+                  onTap: () => launch('tel:2253878327'),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Container(margin: EdgeInsets.only(left: 5.0)),
+                      Icon(Icons.phone, color: Colors.white),
+                      SizedBox(
+                        width: 15.0,
+                      ),
+                      Text('(225) 387-8327',
+                          style: TextStyle(
+                              fontFamily: 'Quicksand',
+                              fontSize: 25.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold)),
+                      Container(margin: EdgeInsets.only(right: 5.0)),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
